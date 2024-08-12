7fa392cfc7c8f6dddd4dd34ca570eee8e49bccb3
Carlos M <cmontano.navarro@gmail.com>
2024-08-12 11:55:30 +0200

    Añadido el nuevo atributo 'fotoMoto' al modelo 'moto'

--- app/Models/moto_model.php
+++ app/Models/moto_model.php
@@ -14,6 +14,7 @@ class moto_model extends Model
     protected $useSoftDeletes = false;
     protected $allowedFields = [
         'matriculaMoto',
+        'fotoMoto',
         'marcaMoto',
         'modeloMoto',
         'cilindradaMoto',
