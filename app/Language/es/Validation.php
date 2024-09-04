be9ecc99fe49fa8eb7b69c9524f0b5c96497214c
Carlos M <cmontano.navarro@gmail.com>
2024-09-04 09:33:49 +0200

    Se modifica locale a es y se crean mensajes de validacion CI4 en español

---
+++ app/Language/es/Validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required' => 'El campo {field} es obligatorio.',
+    'min_length' => 'El campo {field} debe tener al menos {param} caracteres.',
+    'max_length' => 'El campo {field} no puede tener más de {param} caracteres.',
+    'valid_email' => 'El campo {field} debe contener un correo electrónico válido.',
+    'is_unique' => 'El campo {field} ya se encuentra registrado.',
+    'matches' => 'El campo {field} no coincide con {param}.',
+];
